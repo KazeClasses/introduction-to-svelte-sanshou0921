d65adf9f0366e01b9e9e1985e5215dcd579fc09d
sanshou0921 <shianjie009@gmail.com>
2024-10-31 20:19:03 -0400

    Add files via upload

---
+++ src/lib/Hello.svelte
@@ -0,0 +1,8 @@
+<script>
+    let name = $state('world');
+</script>
+{#if name === 'world'}
+<h1 class="text-4xl text-center text-blue-500">Hello {name}!</h1>
+{:else}
+ <h1 class="text-4xl">I don't say hello to anyone else</h1>
+{/if}
